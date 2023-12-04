480d4a27d0150fca85c52451d984409e76e2f7b8
jvm-kbh <elasticsearch@kakao.com>
2023-12-04 15:46:06 +0900

    first commit

---
+++ src/test/java/me/kbh/k8sstudy/K8sStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kbh.k8sstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sStudyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
