34379b6de3484c71751a915bf7c49c241bd59bd1
jvm-kbh <elasticsearch@kakao.com>
2024-01-29 17:45:38 +0900

    setting docker image dog-server

--- src/main/java/me/kbh/k8sstudy/api/dog/DogController.java
+++ src/main/java/me/kbh/k8sstudy/api/dog/DogController.java
@@ -2,11 +2,9 @@ package me.kbh.k8sstudy.api.dog;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/dog")
 public class DogController {
 
   @GetMapping("/name")
