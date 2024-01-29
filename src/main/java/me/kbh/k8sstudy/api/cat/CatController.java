573f1b8e27d448c01dc8426542b064e1679a601c
jvm-kbh <elasticsearch@kakao.com>
2024-01-29 17:42:29 +0900

    setting docker image cat-server

--- src/main/java/me/kbh/k8sstudy/api/cat/CatController.java
+++ src/main/java/me/kbh/k8sstudy/api/cat/CatController.java
@@ -2,11 +2,9 @@ package me.kbh.k8sstudy.api.cat;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/cat")
 public class CatController {
 
   @GetMapping("/name")
